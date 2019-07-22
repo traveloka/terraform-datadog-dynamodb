447297333e2517814a657ba2051f75d32a64d365
I Putu Pradnyana <putu.pradnyana@traveloka.com>
2019-07-22 19:42:27 +0700

    Add default capacity monitoring to datadog

--- output.tf
+++ output.tf
@@ -2,3 +2,13 @@ output "timeboard_title" {
   value       = "${join(",", datadog_timeboard.dynamodb.*.title)}"
   description = "The title of datadog timeboard for DynamoDB"
 }
+
+output "write_capacity_monitor" {
+  value       = "${module.monitor_write_capacity.name}"
+  description = "Name of write capacity monitor"
+}
+
+output "read_capacity_monitor" {
+  value       = "${module.monitor_read_capacity.name}"
+  description = "Name of read capacity monitor"
+}
--- variables.tf
+++ variables.tf
@@ -60,3 +60,52 @@ variable "notify_audit" {
   default     = false
   description = "Whether any configuration changes should be notified"
 }
+
+variable "read_capacity_threshold" {
+  type = "map"
+  default = {
+    ok                = 60
+    warning           = 70
+    warning_recovery  = 65
+    critical          = 80
+    critical_recovery = 75
+  }
+  description = "Thresholds of used read capacity percentage"
+}
+
+variable "read_capacity_message" {
+  type        = "string"
+  default     = ""
+  description = "Message when there is an alert regarding read capacity"
+}
+
+variable "read_capacity_escalation_message" {
+  type        = "string"
+  default     = ""
+  description = "Escalation message for read capacity alert"
+}
+
+
+variable "write_capacity_threshold" {
+  type = "map"
+  default = {
+    ok                = 60
+    warning           = 70
+    warning_recovery  = 65
+    critical          = 80
+    critical_recovery = 75
+  }
+  description = "Thresholds of used write capacity percentage"
+}
+
+variable "write_capacity_message" {
+  type        = "string"
+  default     = ""
+  description = "Message when there is an alert regarding write capacity"
+}
+
+variable "write_capacity_escalation_message" {
+  type        = "string"
+  default     = ""
+  description = "Escalation message for write capacity alert"
+}
